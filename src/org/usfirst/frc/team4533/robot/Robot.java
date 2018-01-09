516017ad80e76401847f48b698818b9f3599c1dd
Ben Duvall <bmduvall10@gmail.com>
2018-01-09 12:05:14 -0500

    Robot.java and Drive Command
    Added OI object to robot.java. Added some autonomous stuff in robot.java but commented it out because we dont have an autonomous mode yet. Finished DriveCommand.

--- src/org/usfirst/frc/team4533/robot/Robot.java
+++ src/org/usfirst/frc/team4533/robot/Robot.java
@@ -3,31 +3,37 @@ package org.usfirst.frc.team4533.robot;
 import org.usfirst.frc.team4533.robot.subsystems.DriveSystem;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class Robot extends IterativeRobot {
 	
 	public static DriveSystem driveSystem;
-	
+	public static OI oi;
+	//public static NameOfAutonomousCommand autonomousCommand;
 	public void robotInit() {
 		driveSystem = new DriveSystem();
+		oi = new OI();
 	}
 	public void disabledInit() {
 		
 	}
 	public void disabledPeriodic() {
-		
+		Scheduler.getInstance().run();
 	}
 	public void autonomousInit() {
 		
 	}
 	public void autonomousPeriodic() {
-		
+		Scheduler.getInstance().run();
 	}
 	public void teleopInit() {
-		
+//		if(autonomousCommand != null) {
+//			autonomousCommand.cancel();
+//		}
 	}
 	public void teleopPeriodic() {
-		
+		Scheduler.getInstance().run();
 	}
 	public void testPeriodic() {
 		
--- src/org/usfirst/frc/team4533/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team4533/robot/commands/DriveCommand.java
@@ -1,13 +1,36 @@
 package org.usfirst.frc.team4533.robot.commands;
 
+import org.usfirst.frc.team4533.robot.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveCommand extends Command {
-
-	@Override
+	
+	private Joystick controller;
+	
+	public DriveCommand() {
+		controller = new Joystick(0);
+		requires(Robot.driveSystem);
+	}
+	
+	protected void execute() {
+		Robot.driveSystem.drivingControls(this.controller);
+	}
+	
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
-
+	
+	protected void end() {
+		
+	}
+	
+	protected void initialize() {
+		
+	}
+	
+	protected void interrupted() {
+		
+	}
 }
